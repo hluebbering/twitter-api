5aafbbcbf28f99e1c9412c1430c2b8cabd4662b6
Luebbering <luebhr@uw.edu>
2022-08-06 11:38:41 -0700

    Add most recent tweets to script

--- scripts/rtweet.R
+++ scripts/rtweet.R
@@ -11,15 +11,22 @@ library(tidytext)
 library(quanteda)
 library(hrbrthemes)
 library(httr)
+# library(academictwitteR)
+library(rtweetXtras)
+# install_version("rtweet", version = "0.7.0")
 
-api_key1 = "HqET15WxmS965qTH2q5u8tJtI"
-api_key_secret1 = "uAH8a2MNgRUT043js8SuhiAW1VFDuOFq5KjMuIUwa7rJtsEF2Q"
-access_token1 = "1357218984798609409-h8nBD6Hr8ulTidM55CEqq5n95Haa2e"
-access_token_secret1 = "2UyY24reVECUidf8x97o9mrxkA7EGn6y8z8tyCk8Ie9UB"
 
-bearer_toke = "AAAAAAAAAAAAAAAAAAAAAA9AbwEAAAAAvE6p2MrtLewTfUKMvN8f45kBgmg%3Dq0KVRZOG9uA3Ahhp41Hkirpg7pS0HHsoD7H3WTILajhptiyk8e"
-client_id = "YXY3Y2xDa3czM3FxVm83aEZ6YXM6MTpjaQ"
-client_secret = "tNmcI6uGBfbi8zV9AUKmzeOZbZ-B9jKlkDnaOKPiKMop61r5j6"
+
+api_key1 <- "AD6QMEvWQGztogOmoVHEkWLoE"
+api_key_secret1 <- "BsihaN82bsfHkZxnAD2a9s4m0amKewB4hFdf94erAXQyLhoWjQ"
+access_token1 <- "1357218984798609409-WeWWKQ9ucDaJTCl8DH4X0sohI5uXVc"
+access_token_secret1 <- "PcWLNnAJwFTKM5WrIWomaDHuadaW35JwRRcSPartYCdh6"
+
+bearer_toke <- "AAAAAAAAAAAAAAAAAAAAAA9AbwEAAAAA6zhbH%2Bbhnw1ytRVAl9rQhEypn0A%3Duhy92utPtyr1cVdgQgHR6wCf4nKYgyVVGKXJOq3GV7zzeh3JAH"
+
+
+client_id <- "YXY3Y2xDa3czM3FxVm83aEZ6YXM6MTpjaQ"
+client_secret <- "v-ow4p7dYZws_VnJELslgrzlkcrhq_O0qVjetCLM9U96XjcVW7"
 
 twitter_token <- create_token(
   app = "LuebberingApp",
@@ -31,16 +38,15 @@ twitter_token <- create_token(
 )
 use_oauth_token(twitter_token)
 
-
 # users <- "elonmusk"
 # 
-# musktweets7 <-
+# musktweets8 <-
 #   search_fullarchive(
 #     paste0("from:", users, collapse = " OR "),
 #     n = 2000,
-#     env_name = "devv",
-#     fromDate = "202205272255",
-#     toDate = "202206210000")
+#     env_name = "devenviroment",
+#     fromDate = "202206210000",
+#     toDate = "202208050000")
 
 # musktweets5 <-
 #   search_fullarchive(
@@ -50,5 +56,5 @@ use_oauth_token(twitter_token)
 #     fromDate = "201001010000",
 #     toDate = "201201010000")
 
-# save_as_csv(musktweets7, file_name = "data/musktweets202206.csv")
+# save_as_csv(musktweets8, file_name = "data/musktweets202208.csv")
 
